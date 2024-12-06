0ab7d3ef1519c517d9a4b66ce61b0f406db0a987
Stefano Suduhakuruge <stefano.sanka@gmail.com>
2024-12-06 08:29:20 +0000

    added comments to the solution :D

--- day5/print-queue.ts
+++ day5/print-queue.ts
@@ -37,21 +37,25 @@ async function readFile() {
             let ptr2 = ptr1 + 1;
 
             if (update[ptr1] in dict) {
+                // check if ptr2 is in array of key ptr2 in update and dict else break
                 if (dict[update[ptr1]].includes(update[ptr2])) {
+                    // while true check the next item in the array
                     while (dict[update[ptr1]].includes(update[ptr2])) {
                         ptr2+=1;
                     } 
                 } else {
                     break;
                 }
+            } else {
+                // early break no need to check if statement below, move to next update
+                break;
             }
 
+            // if ptr1 has reached second to last item in update and ptr2 is at the last item in update
             if (ptr1 == update.length - 2 && ptr2 == update.length) {
                 middleSum += update[(update.length-1)/2];
             }
         }
     }
-
     console.log(middleSum);
-
 })(); 
